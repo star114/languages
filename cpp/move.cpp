33a564602637661efce535259a251bab8ca4c288
Heesik Shin <start114.shin@gmail.com>
2017-04-10 11:05:12 +0900

    move : -fno-elide-constructors

--- cpp/move.cpp
+++ cpp/move.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
+static int test = 0;
 class A
 {
 public:
-    A() : _a(0) { std::cout << "A::A()" << std::endl; }
-    explicit A(int a) : _a(a) { std::cout << "A::A(int)" << std::endl; }
-    A(const A& rhs) : _a(rhs._a) { std::cout << "A::Copy constructor" << std::endl; }
-    A(A&& rhs) : _a(std::move(rhs._a)) { std::cout << "A::Move constructor" << std::endl; }
+    A() : _a(0) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    explicit A(int a) : _a(a) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    A(const A& rhs) : _a(rhs._a) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    A(A&& rhs) : _a(std::move(rhs._a)) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    ~A() { std::cout << __PRETTY_FUNCTION__ << std::endl; test++; }
 
     int get() const { return _a; }
 private:
@@ -16,10 +18,11 @@ private:
 class B
 {
 public:
-    B() : _b(0) { std::cout << "B::B()" << std::endl; }
-    explicit B(int b) : _b(b) { std::cout << "B::B(int)" << std::endl; }
-    B(const B& rhs) : _b(rhs._b) { std::cout << "B::Copy constructor" << std::endl; }
-    B(B&& rhs) : _b(std::move(rhs._b)) { std::cout << "B::Move constructor" << std::endl; }
+    B() : _b(0) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    explicit B(int b) : _b(b) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    B(const B& rhs) : _b(rhs._b) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    B(B&& rhs) : _b(std::move(rhs._b)) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    ~B() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
 
     void print() { std::cout << _b.get() << std::endl; }
 private:
@@ -35,10 +38,13 @@ B print(B&& b)
 
 int main()
 {
-    B b(1);
-    //B b(std::move(B(1)));
+    {
+        B b(B(1));
+        //B b(std::move(B(1)));
 
-    print(std::move(b));
-    b.print();
+        //print(std::move(b));
+        //b.print();
+    }
+    std::cout << test << std::endl;
     return 0;
 }
