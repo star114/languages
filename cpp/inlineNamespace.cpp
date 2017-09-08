95407f5f6e96a0f496052cd0b7348006b17176c1
Shin Heesik <sinheesik@gmail.com>
2017-09-08 22:25:56 +0900

    cpp11

---
+++ cpp/inlineNamespace.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+namespace global {
+
+inline
+namespace a {
+void f()
+{
+    std::cout << "a::f()" << std::endl;
+}
+}
+
+inline
+namespace b {
+void f()
+{
+    std::cout << "b::f()" << std::endl;
+}
+}
+
+void func()
+{
+    f();
+}
+}
+
+int main()
+{
+    global::func();
+    return 0;
+}
+
---
+++ cpp/variodic.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+template <typename T, typename ... Params> decltype(auto) f1(T, P:
